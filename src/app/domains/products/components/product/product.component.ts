9560ca4825492331047fc902855786845f2dc9b9
DavidR <omar.rebolledo@visionymarketing.com.co>
2024-03-20 15:24:11 -0500

    Usando librerias: se uso una libreria la cual calculaba el tiempo entre hoy y de publicacion

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,12 +1,14 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '@shared/models/product.model';
 import { ReversePipe } from '@shared/pipes/reverse.pipe';
-import { UpperCasePipe , CurrencyPipe, DatePipe } from '@angular/common';
+import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';
+
+import { UpperCasePipe , CurrencyPipe, DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [UpperCasePipe, CurrencyPipe, DatePipe, ReversePipe],
+  imports: [UpperCasePipe, CurrencyPipe, DatePipe, ReversePipe,TimeAgoPipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
